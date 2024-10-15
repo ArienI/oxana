72f586038a416bbd4f764ea501ce9b53635d2c84
ArienI <Laurilin2022@gmail.com>
2024-10-15 04:10:40 +0200

    fix:img

--- src/js/main.js
+++ src/js/main.js
@@ -1,21 +1,14 @@
 import {
   initSiteSettings
 } from './modules/settings.js';
-import {
-  initLangDropdown
-} from './modules/lang-dropdown.js';
-import {
-  initHeaderMenu
-} from './modules/header-menu.js';
+
 import {
   initSliders
 } from './modules/slider.js';
 
-document.addEventListener( 'DOMContentLoaded', () => {
+document.addEventListener('DOMContentLoaded', () => {
   initSiteSettings();
-  window.addEventListener( 'load', () => {
-    initHeaderMenu();
-    initLangDropdown();
+  window.addEventListener('load', () => {
     initSliders();
-  } );
-} );
+  });
+});
